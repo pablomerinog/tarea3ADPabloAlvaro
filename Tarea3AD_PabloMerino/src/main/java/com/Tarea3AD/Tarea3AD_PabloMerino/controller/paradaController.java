17aacaeb718829aca516f2ad3719495ace6e61ff
alumnoDAM <alumnoDAM@DESKTOP-PRRG66G>
2025-05-15 19:27:52 +0200

    cambios

--- Tarea3AD_PabloMerino/src/main/java/com/Tarea3AD/Tarea3AD_PabloMerino/controller/paradaController.java
+++ Tarea3AD_PabloMerino/src/main/java/com/Tarea3AD/Tarea3AD_PabloMerino/controller/paradaController.java
@@ -1,36 +1,19 @@
 package com.Tarea3AD.Tarea3AD_PabloMerino.controller;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
-import java.util.Set;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
-import org.w3c.dom.DOMImplementation;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.ProcessingInstruction;
 
 import com.Tarea3AD.Tarea3AD_PabloMerino.config.StageManager;
 import com.Tarea3AD.Tarea3AD_PabloMerino.modelo.Carnet;
-import com.Tarea3AD.Tarea3AD_PabloMerino.modelo.Estancia;
 import com.Tarea3AD.Tarea3AD_PabloMerino.modelo.Parada;
-import com.Tarea3AD.Tarea3AD_PabloMerino.modelo.PereParada;
 import com.Tarea3AD.Tarea3AD_PabloMerino.modelo.Peregrino;
 import com.Tarea3AD.Tarea3AD_PabloMerino.modelo.Sesion;
 import com.Tarea3AD.Tarea3AD_PabloMerino.modelo.Usuario;
@@ -48,6 +31,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -107,6 +92,12 @@ public class paradaController implements Initializable {
 	@FXML
 	private DatePicker fechaFin;
 
+	@FXML
+	private CheckBox checkVIP;
+
+	@FXML
+	private ComboBox<String> cmbxPeregrinos;
+
 	@Autowired
 	private UserService userService;
 
@@ -121,21 +112,19 @@ public class paradaController implements Initializable {
 	@Lazy
 	@Autowired
 	private StageManager stageManager;
-	
+
 	private ObservableList<Parada> paradaList = FXCollections.observableArrayList();
-	private ObservableList<Peregrino> pereList = FXCollections.observableArrayList();
 
 	@Autowired
 	private Sesion sesion;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-
-		colIdPere.setCellValueFactory(new PropertyValueFactory<>("id"));
-		colNacionalidadPere.setCellValueFactory(new PropertyValueFactory<>("nacionalidad"));
-		colNombrePere.setCellValueFactory(new PropertyValueFactory<>("nombrePeregrino"));
-		colCarnetPere.setCellValueFactory(new PropertyValueFactory<>("carnet"));
-		colUsuarioPere.setCellValueFactory(new PropertyValueFactory<>("usuario"));
+		colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		colNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+		colRegion.setCellValueFactory(new PropertyValueFactory<>("region"));
+		colResp.setCellValueFactory(new PropertyValueFactory<>("responsable"));
+		colUsuario.setCellValueFactory(new PropertyValueFactory<>("idUsuario"));
 
 		cargarPeregrinos();
 		cargarParadas();
@@ -145,12 +134,16 @@ public class paradaController implements Initializable {
 		paradaList.clear();
 		paradaList.addAll(paradasService.findAll());
 		tablaParadas.setItems(paradaList);
+
 	}
 
-	public void cargarPeregrinos() {
-		pereList.clear();
-		pereList.addAll(pereService.findAll());
-		tablaPeregrinos.setItems(pereList);
+	private void cargarPeregrinos() {
+		List<Peregrino> peregrinos= pereService.findAll();
+		List<String> nombresPeregrinos = new ArrayList<>();
+		for (Peregrino p : peregrinos) {
+			nombresPeregrinos.add(p.getNombrePeregrino());
+		}
+		cmbxPeregrinos.setItems(FXCollections.observableArrayList(nombresPeregrinos));
 	}
 
 	public void alertaInfo(String title, String message) {
@@ -173,8 +166,29 @@ public class paradaController implements Initializable {
 	}
 
 	@FXML
-	private void buscarFechas(ActionEvent event) throws IOException {
-		System.out.println("no esta");
+	private void sellarParada(ActionEvent event) throws IOException {
+
+		Usuario usuario = sesion.getUsuIniciado();
+
+		System.out.println("Usuario: " + usuario.getnombreUsuario());
+
+		Optional<Parada> paradaOpt = paradasService.findByNombre(usuario.getnombreUsuario());
+		if (paradaOpt.isEmpty()) {
+			System.out.println("El responsable no tiene paradas " + usuario.getnombreUsuario());
+			return;
+		}
+
+		String pere = cmbxPeregrinos.getValue();
+		Optional<Peregrino> paradaInicialOpt = pereService.findBynombrePeregrino(pere);
+		
+		boolean isVip = false;
+
+		if (checkVIP.isSelected()) {
+			isVip = true;
+		} else {
+			isVip = false;
+		}
+
 	}
 
 }
