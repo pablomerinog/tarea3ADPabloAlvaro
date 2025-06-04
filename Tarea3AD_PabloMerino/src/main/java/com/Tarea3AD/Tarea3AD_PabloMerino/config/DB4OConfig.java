e4ef2f3d67511f75d2d18038d7bc933b22549048
= <=>
2025-06-04 17:01:38 +0200

    objectDB

---
+++ Tarea3AD_PabloMerino/src/main/java/com/Tarea3AD/Tarea3AD_PabloMerino/config/DB4OConfig.java
@@ -0,0 +1,33 @@
+package com.Tarea3AD.Tarea3AD_PabloMerino.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.Tarea3AD.Tarea3AD_PabloMerino.modelo.ConjuntoContratado;
+import com.Tarea3AD.Tarea3AD_PabloMerino.modelo.Servicio;
+import com.Tarea3AD.Tarea3AD_PabloMerino.repository.Db4oRepository;
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+
+@Configuration
+public class DB4OConfig {
+
+	@Configuration
+	public class Db4oConfig {
+
+		@Bean(destroyMethod = "close")
+		public ObjectContainer db4oContainer() {
+			return Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "peregrinos.db4o");
+		}
+
+		@Bean
+		public Db4oRepository<Servicio> servicioDb4oRepository(ObjectContainer db4oContainer) {
+			return new Db4oRepository<>(db4oContainer, Servicio.class);
+		}
+
+		@Bean
+		public Db4oRepository<ConjuntoContratado> conjuntoContratadoDb4oRepository(ObjectContainer db4oContainer) {
+			return new Db4oRepository<>(db4oContainer, ConjuntoContratado.class);
+		}
+	}
+}
--- Tarea3AD_PabloMerino/src/main/java/com/Tarea3AD/Tarea3AD_PabloMerino/config/ObjectDBConfig.java
+++ Tarea3AD_PabloMerino/src/main/java/com/Tarea3AD/Tarea3AD_PabloMerino/config/ObjectDBConfig.java
@@ -1,33 +1,33 @@
 package com.Tarea3AD.Tarea3AD_PabloMerino.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import java.io.IOException;
+import java.util.Properties;
 
-import com.Tarea3AD.Tarea3AD_PabloMerino.modelo.ConjuntoContratado;
-import com.Tarea3AD.Tarea3AD_PabloMerino.modelo.Servicio;
-import com.Tarea3AD.Tarea3AD_PabloMerino.repository.Db4oRepository;
-import com.db4o.Db4oEmbedded;
-import com.db4o.ObjectContainer;
 
-@Configuration
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class ObjectDBConfig {
 
-	@Configuration
-	public class Db4oConfig {
+	private static final EntityManagerFactory emf;
 
-		@Bean(destroyMethod = "close")
-		public ObjectContainer db4oContainer() {
-			return Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "peregrinos.db4o");
+	static {
+		Properties properties = new Properties();
+		try {
+			properties.load(ObjectDBConfig.class.getClassLoader().getResourceAsStream("application.properties"));
+		} catch (IOException e) {
+			throw new RuntimeException("No se pudo cargar el archivo application.properties", e);
 		}
+		String dbPath = properties.getProperty("odb.path");
+		emf = Persistence.createEntityManagerFactory(dbPath);
+	}
 
-		@Bean
-		public Db4oRepository<Servicio> servicioDb4oRepository(ObjectContainer db4oContainer) {
-			return new Db4oRepository<>(db4oContainer, Servicio.class);
-		}
+	public static EntityManager getEntityManager() {
+		return emf.createEntityManager();
+	}
 
-		@Bean
-		public Db4oRepository<ConjuntoContratado> conjuntoContratadoDb4oRepository(ObjectContainer db4oContainer) {
-			return new Db4oRepository<>(db4oContainer, ConjuntoContratado.class);
-		}
+	public static void close() {
+		emf.close();
 	}
 }
